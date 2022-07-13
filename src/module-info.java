5e00f1312e3c873d3eb1902aa2afaa5543fcb7d4
tinycodecrank <tinycodecrank@gmail.com>
2022-07-13 16:37:39 +0200

    update Module name

--- src/module-info.java
+++ src/module-info.java
@@ -1,8 +1,8 @@
-module IOUtils
+module de.tinycodecrank.IOUtils
 {
 	exports de.tinycodecrank.util.io.stream;
 
-	requires Functional;
-	requires transitive Monads;
-	requires mathUtils;
+	requires de.tinycodecrank.Functional;
+	requires transitive de.tinycodecrank.Monads;
+	requires de.tinycodecrank.mathUtils;
 }
